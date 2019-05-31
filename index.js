25eee7bba631c73807b9d12653f92b0be98b969b
Sasha <taylork85@gmail.com>
2019-05-30 19:07:16 -0500

    correct port variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { server } = require('./config/server.js');
 
 
 
-const port = process.env.port || 7000;
+const port = process.env.PORT || 7000;
 server.listen(port, () => {
     console.log(`Server started on port ${port}!`)
 })
